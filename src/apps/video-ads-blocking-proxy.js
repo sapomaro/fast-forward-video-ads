c798b8778b50f8ca46a78e44f58eda0c74007af1
Optimizitor <sapomaro@gmail.com>
2024-06-02 15:47:40 +0300

    feat: improve script injection

--- src/apps/video-ads-blocking-proxy.js
+++ src/apps/video-ads-blocking-proxy.js
@@ -17,25 +17,47 @@ export function runVideoAdsBlockingProxy() {
         target: HOST_MAIN,
         destination: IP,
         pathRewrite: { '^/go': PATH_MAIN },
-        amendIncomingHtml: `
-            <script>
-                function trimMainVideoIframe() {
-                    const collection = document.getElementsByTagName('iframe') || [];
-                    for (const item of collection) {
-                        if (item.src?.includes('${HOST_CDN}'.replace(/^.+\\.([A-Za-z0-9]+\\.[A-Za-z]+\\/?)$/, '$1'))) {
-                            const newSrc = item.src.replace(/https:\\/\\/[^/]+\\//g, 'http://${IP}:${PORT_CDN}/');
-                            document.body.innerHTML = '<iframe id="video_iframe" width="840" height="600" src="' + newSrc + '" frameborder="0" allowfullscreen=""></iframe>';
-                            break;
-                        }
-                    }
-                    document.body.setAttribute('style', 'background: #000 !important; min-height: 100vh !important; display: flex !important; align-items: center !important; justify-content: center !important;');
-                }
+        followRedirects: false,
+        amendIncomingHtml: (html) => {
+            let headIndex = html.indexOf('<head>');
+            if (headIndex === -1) {
+                headIndex = 0;
+            } else {
+                headIndex += 6;
+            }
 
-                document.addEventListener('DOMContentLoaded', () => {
-                    setTimeout(trimMainVideoIframe, ${FIRST_LOAD_DELAY_MS});
-                });
-            </script>
-        `,
+            const firstPart = html.slice(0, headIndex) + `
+                <script>
+                    function trimMainVideoIframe() {
+                        const collection = document.getElementsByTagName('iframe') || [];
+                        for (const item of collection) {
+                            if (item.src?.includes('${HOST_CDN}'.replace(/^.+\\.([A-Za-z0-9]+\\.[A-Za-z]+\\/?)$/, '$1'))) {
+                                const newSrc = item.src.replace(/https:\\/\\/[^/]+\\//g, 'http://${IP}:${PORT_CDN}/');
+                                document.body.innerHTML = '<iframe id="video_iframe" width="840" height="600" src="' + newSrc + '" frameborder="0" allowfullscreen=""></iframe>';
+                                break;
+                            }
+                        }
+                        document.body.setAttribute('style', 'background: #000 !important; min-height: 100vh !important; display: flex !important; align-items: center !important; justify-content: center !important;');
+                    }
+
+                    document.addEventListener('DOMContentLoaded', () => {
+                        setTimeout(trimMainVideoIframe, ${FIRST_LOAD_DELAY_MS});
+                    });
+
+                    window.onbeforeunload = function(){
+                        return 'Are you sure you want to redirect?';
+                    };
+
+                    window.location_ = {};
+                </script>
+            `;
+
+            const secondPart = html.slice(headIndex)
+                .replace(/([^a-z0-9_])(location)([^a-z0-9_])/gi, '$1$2_$3')
+                .replace(/([^a-z0-9_])(throw)([^a-z0-9_])/gi, '$1$2_=$3');
+
+            return firstPart + secondPart;
+        },
     }));
 
     mainProxyServer.listen(PORT_MAIN, () => {
